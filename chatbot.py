f320fc9648030bd780e659f13fa9e8d106c63fbd
Christopher Song <chris4728.song2001@gmail.com>
2023-02-27 16:33:58 -0600

    Basic chatbot now works end-to-end

--- chatbot.py
+++ chatbot.py
@@ -13,12 +13,8 @@ def main(data_source):
     recipe_data = Recipe(data_source)
     print("Done loading recipe data!\n")
 
-    # print(recipe_data.ingredient_quantities)
-    # for s in recipe_data.parsed_steps:
-    #   s.print()
-
     idx = 0
-    outputStep(recipe_data.steps, idx)
+    output_step(recipe_data.steps, idx)
 
     while True:
         x = input("Input: ").lower()
@@ -42,8 +38,12 @@ def main(data_source):
               for result in results:
                   print(result)
             else:
-              # Ingredient quantity lookup
-              pass
+              quantity = ingredient_lookup(recipe_data.ingredient_quantities, match[1])
+
+              if quantity is None:
+                print(f"Response: Unsure how much quantity needed for ingredient '{match[1]}'; please try another query")
+              else:
+                print(quantity)
 
             valid_input = True
             break
@@ -52,33 +52,60 @@ def main(data_source):
           for i, match in enumerate(exact_matches):
             if x == match:
               if i == 0:
-                # Temp lookup
-                pass
+                current_temperature = recipe_data.parsed_steps[idx].temperature
+
+                if current_temperature is None:
+                  print("Response: No temperature associated with this recipe step")
+                else:
+                  print("Response:", current_temperature)
               elif i == 1:
-                # Time lookup
-                pass
+                current_time = recipe_data.parsed_steps[idx].time
+
+                if current_time is None:
+                  print("Response: No time amount associated with this recipe step")
+                else:
+                  print("Response:", current_time)
               elif i == 2:
                 idx += 1
-                outputStep(recipe_data.steps, idx)
+                output_step(recipe_data.steps, idx)
               elif i == 3:
+                if idx == 0:
+                  print("Response: Cannot go back from the first step")
+                  valid_input = True
+                  break
+
                 idx -= 1
-                outputStep(recipe_data.steps, idx)
+                output_step(recipe_data.steps, idx)
               else:
-                outputStep(recipe_data.steps, idx)
+                output_step(recipe_data.steps, idx)
 
               valid_input = True
               break
 
         if not valid_input:
-          # Print some error message
-          pass
+          print_error_message()
 
-def outputStep(step_list, idx):
-    if idx < 0 or idx >= len(step_list):
-        print(f"Response: Out of steps at step: {idx}")
+def ingredient_lookup(quantities, query):
+  for ingredient, quantity in quantities.items():
+    if query in ingredient:
+      return quantity
+
+  return None
+
+def output_step(step_list, idx):
+    if idx == len(step_list):
+      print("Response: Done with recipe, chatbot closing now")
+      exit(0)
 
     extra_output = f"(First Step)" if idx == 0 else "(Last Step)" if idx == len(step_list) - 1 else ""
     print(f"Response: {extra_output} {step_list[idx]}")
+
+def print_error_message():
+  print()
+  print("Sorry, we didn't recognize your input. Please ask a question in one of the following forms (case insensitive):\n")
+  print("1. How do I (.*)\n2. What is (.*)\n3. How much (.*) do I need\n4. What temperature")
+  print("5. How long\n6. Next\n7. Back\n8. Repeat")
+  print()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
